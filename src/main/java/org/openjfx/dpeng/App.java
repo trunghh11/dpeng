d81ad613df4cb72e9bbabd03e19d2f36750667fb
trungtrex <trung291004@gmail.com>
2023-10-29 03:46:27 +0700

    FIX BUG SLIDING MENU 29 OCTOBER

--- src/main/java/org/openjfx/dpeng/App.java
+++ src/main/java/org/openjfx/dpeng/App.java
@@ -2,7 +2,6 @@ package org.openjfx.dpeng;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
